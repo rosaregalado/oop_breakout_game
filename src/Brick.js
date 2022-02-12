b3b021d3b14dc6ba58185f03c024a739ed942e62
Rosa Regalado <rosaregalado101@gmail.com>
2022-02-11 19:03:00 -0800

    removed node modules

--- src/Brick.js
+++
@@ -1,9 +0,0 @@
-import Sprite from "./Sprite.js";
-
-class Brick extends Sprite {
-  constructor(x, y, width = 75, height = 20, color = '#0095DD') {
-    super(x, y, width, height, color)
-    this.status = true;
-  }
-}
-export default Brick;
